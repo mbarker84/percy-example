94886888da080b906e0d376b2861614ab1b5d3c0
Michelle Barker <mbarker_84@hotmail.co.uk>
2020-02-25 14:17:46 +0000

    Change url

--- snapshots.js
+++ snapshots.js
@@ -2,7 +2,7 @@ const PercyScript = require("@percy/script");
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto("https://confident-yonath-b6fd80.netlify.com/");
+  await page.goto(process.env.PERCY_URL);
   await page.waitFor(".is-loaded");
   await percySnapshot("TodoMVC home page");
 });
